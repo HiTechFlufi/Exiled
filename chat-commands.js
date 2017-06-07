818a9f03aa860b4bf726c316e5a911eafe35bc4b
HiTechFlufi <zach.hartin@gmail.com>
2017-06-07 19:12:21 -0400

    Fix #2?
    Changing Let to Const. It says Let is a reserved keyword, not sure what that means.

--- chat-commands.js
+++ chat-commands.js
@@ -76,17 +76,17 @@ exports.commands = {
 	//Coded by flufi for testing purposes.
 	cf: 'cflip',
 	cflip: function (target, room, user, connection, cmd) {
-		let flipping = true;
+		const flipping = true;
 		this.sendReply('Choose a side. (Use either /cf heads, or /cf tails)');
 		if (flipping === true && cmd === 'heads') {
-			let side = 'heads';
+			const side = 'heads';
 		} else if (flipping === true && cmd === 'tails') {
-			let side = 'tails';
+			const side = 'tails';
 		} else {
 			return this.errorReply('That is not a valid coin side.');
 		}
 		
-		let f = this.random(2); //Rolling for Heads or Tails, 1 is heads, 2 is tails
+		const f = this.random(2); //Rolling for Heads or Tails, 1 is heads, 2 is tails
 		if (f === 1 && flipping === true && side === 'heads') {
 			this.sendReply('You chose Heads. The Result; Heads! You win!');
 		} else if (f === 1 && flipping === true && side === 'tails') {
@@ -97,7 +97,7 @@ exports.commands = {
 			this.sendReply('You chose Tails. The result; Tails! You win!');
 		}
 		
-		return let flipping = false;
+		return const flipping = false;
 	},
 
 	'!battle': true,
